099586793f37e4429f53a9dcadc51d1b39212312
Youssef <youssefaridhi8@gmail.com>
2025-04-23 21:23:01 +0200

    fix bug

--- libs/shared/src/shared.module.ts
+++ libs/shared/src/shared.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { SharedService } from './shared.service';
 import { JwtGuard } from './guards/jwt.guard';
 import { JwtStrategy } from './strategy/jwt.strategy';
+import { AdminGuard } from './guards/adminGuard';
 
 @Module({
-  providers: [JwtGuard, JwtStrategy],
-  exports: [JwtGuard, JwtStrategy],
+  providers: [JwtGuard, JwtStrategy, AdminGuard],
+  exports: [JwtGuard, JwtStrategy, AdminGuard],
 })
 export class SharedModule {}
